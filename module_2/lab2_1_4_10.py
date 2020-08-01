4eb872a669bba8038722827bb094c9aa793307e4
dzooli <zoltan.dzooli.fabian@gmail.com>
2020-08-02 00:46:55 +0200

    Variables and basic string operations + console input

---
+++ module_2/lab2_1_4_10.py
@@ -0,0 +1,13 @@
+import pydoc
+for x in (0., 1.0, -1.0):
+    y = (3*x**3) - (2*x**2)+(3*x)-1
+    print("y =", y)
+
+# Python is dinamically typed
+# You can use compound operators like in C (+=. -=, ect.)
+
+""" 
+Multi line comments also
+possible in Python as 
+docstrings
+"""
---
+++ module_2/lab2_1_6_1_input.py
@@ -0,0 +1,4 @@
+leg_a = float(input('Side1 length: '))
+leg_b = float(input('Side2 length: '))
+calc = (leg_a**2 + leg_b**2) ** .5
+print("Third's length is", calc)
