fd884e301c03671cce3a3c7bfcb0fd5d83ca0f95
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2023-02-10 22:50:34 +0100

    relaunch

---
+++ src/relaunch/2023/src/part1_examples.py
@@ -0,0 +1,26 @@
+instructions = """
+
+Output should be:
+
+"I'm"
+""learning""
+"\"\"Python\"\"\"
+
+Results:
+"""
+print(instructions)
+print('"I\'m"\n""learning""\n"""Python"""')
+
+
+instructions = """
+
+Integer division: //
+This is very important: rounding always goes to the lesser integer.
+
+print(-6 // 4)
+print(6. // -4)
+
+"""
+print(instructions)
+print(-6 // 4)
+print(6. // -4)
