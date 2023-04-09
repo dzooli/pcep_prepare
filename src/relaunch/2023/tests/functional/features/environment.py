9e59605cc6140021d8947ba60ed9f27502739ca8
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2023-04-09 18:40:53 +0200

    cleanup

--- src/relaunch/2023/tests/functional/features/environment.py
+++ src/relaunch/2023/tests/functional/features/environment.py
@@ -1,7 +1,5 @@
 import os
 import sys
-from pprint import pprint
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..\\..\\..\\src"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../../src"))
-
