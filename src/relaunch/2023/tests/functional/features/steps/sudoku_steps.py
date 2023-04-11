d40b0345891a2a5f0ef65366ca2245d16fc32c16
Zoltan Fabian <zoltan.dzooli.fabian@gmail.com>
2023-04-11 18:25:41 +0200

    module2 - tests cleanup

--- src/relaunch/2023/tests/functional/features/steps/sudoku_steps.py
+++ src/relaunch/2023/tests/functional/features/steps/sudoku_steps.py
@@ -1,5 +1,4 @@
-from pprint import pprint
-from behave import use_step_matcher, Then
+from behave import use_step_matcher, Then, When, Given
 from part2_pcap.module_2 import sudoku
 
 use_step_matcher("parse")
@@ -39,3 +38,24 @@ def valid_invalid_table(context, valid):
         assert not is_valid, "Table is valid but expected to be not valid!"
     else:
         assert False, f"Invalid keyword in the gherkin file: {valid}"
+
+
+@Given("Table input from the user")
+def table_input(context):
+    sudoku.input_table()
+
+
+@Then("Sudoku table is empty")
+@Given("Sudoku table is empty")
+def table_is_empty(context):
+    assert len(sudoku.table) == 0, "Table is not empty!"
+
+
+@When("I initialize the table with this {data}")
+def fill_table(context, data):
+    sudoku.init_table(data)
+
+
+@Given("An empty Sudoku table")
+def cleanup_table(context):
+    sudoku.clean_table(sudoku.table)
